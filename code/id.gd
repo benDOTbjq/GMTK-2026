5e5f54fb97bb11abf17aa7d35ba73b61d5de62e7
Benjamin Q <61244192+benDOTbjq@users.noreply.github.com>
2026-07-23 18:49:31 +0100

    id

---
+++ code/id.gd
@@ -0,0 +1,22 @@
+class_name ID extends RefCounted
+
+enum DemonType {
+	FERRIC = 0,
+	CHTHONIC = 1,
+	INFERNAL = 2,
+	SPECTRAL = 3,
+	LUCIFERIAN = 4,
+	PUTREFACTIOUS = 5,
+	HADEL = 6,
+	THAUMIC = 7,
+	ENUM_COUNT = 8,
+	NONE = 9
+}
+
+enum Item {
+	HOLY_WATER = 0,
+	SALT = 1,
+	IRON = 2,
+	PRAYER = 3,
+	ENUM_COUNT = 4
+}
