1d7d2f8890253f519763802337530b0642a4e8ff
Benjamin Q <61244192+benDOTbjq@users.noreply.github.com>
2026-07-23 19:05:23 +0100

    text output

--- code/demo_scene/gui.gd
+++ code/demo_scene/gui.gd
@@ -9,6 +9,7 @@ var _current_demon: Dictionary
 @onready var prayer_button: Button = %PrayerButton
 @onready var reset_button: Button = %ResetButton
 @onready var check_button: Button = %CheckButton
+@onready var label: Label = %Label
 
 
 func _ready() -> void:
@@ -24,12 +25,26 @@ func _ready() -> void:
 
 func _reset_demon() -> void:
 	_current_demon = DemonManager.get_random_demon()
+	label.text = ""
 
 
 func _print_demon() -> void:
+	label.text = (
+		DemonManager.TYPE_TO_STRING[_current_demon.type_1] + 
+		", " +
+		DemonManager.TYPE_TO_STRING[_current_demon.type_2] +
+		"\n" +
+		str(_current_demon.item_effectiveness)
+	)
 	print(DemonManager.TYPE_TO_STRING[_current_demon.type_1], ", ", DemonManager.TYPE_TO_STRING[_current_demon.type_2])
 	print(_current_demon.item_effectiveness)
 
 
 func _test_item(item_id: ID.Item) -> void:
 	print(_current_demon.item_effectiveness[item_id])
+	match _current_demon.item_effectiveness[item_id]:
+		-2: label.text = "The demon is\nEmpowered"
+		-1: label.text = "The demon is\nUneffected"
+		0: label.text = "The demon is\nMildly effected"
+		1: label.text = "The demon is\nStrongly effected"
+		2: label.text = "The demon is\nExtremely effected"
--- code/demo_scene/print_types.gd
+++ code/demo_scene/print_types.gd
@@ -24,9 +24,9 @@ enum ItemType {
 
 func print_types() -> void:
 	var combo_types: Dictionary[int, Vector4]
-	for type_1 in DemonManager.TYPE_REACTIONS.keys():
-		for type_2 in DemonManager.TYPE_REACTIONS.keys():
-			combo_types[type_1 + type_2] = DemonManager.TYPE_REACTIONS[type_1] + DemonManager.TYPE_REACTIONS[type_2]
+	for type_1 in DemonManager.DEMON_TYPE_EFFECTIVENESS.keys():
+		for type_2 in DemonManager.DEMON_TYPE_EFFECTIVENESS.keys():
+			combo_types[type_1 + type_2] = DemonManager.DEMON_TYPE_EFFECTIVENESS[type_1] + DemonManager.DEMON_TYPE_EFFECTIVENESS[type_2]
 	for type_1 in combo_types.keys():
 		print(type_1, combo_types[type_1])
 	print(combo_types.size())
