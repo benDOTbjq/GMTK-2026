7a0abe8b0b12f69f1c8bc709cc96d4668b3c4d79
Lucas Magalhaes <60627375+freddder@users.noreply.github.com>
2026-07-24 12:24:27 -0400

    Added interactable items on the table

---
+++ code/item.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var item_type: ID.Item
+
+@onready var mesh : MeshInstance3D = $MeshInstance3D
+var is_hovered : bool = false
+
+func _ready() -> void:
+	$Area3D.input_event.connect(_on_area_3d_input_event)
+	$Area3D.mouse_entered.connect(_on_area_3d_mouse_entered)
+	$Area3D.mouse_exited.connect(_on_area_3d_mouse_exited)
+
+func _on_area_3d_mouse_entered() -> void:
+	is_hovered = true
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+func _on_area_3d_mouse_exited() -> void:
+	is_hovered = false
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var gui : GUI = $"../GUI"
+		gui._test_item(item_type)
