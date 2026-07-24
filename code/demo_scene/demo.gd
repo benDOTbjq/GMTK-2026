63bbdd528d21ba12f70924fd877f589775a8217e
Lucas Magalhaes <60627375+freddder@users.noreply.github.com>
2026-07-24 00:56:28 -0400

    Added shadow spawn animation

--- code/demo_scene/demo.gd
+++ code/demo_scene/demo.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
-
 @onready var gui: GUI = $GUI
 @onready var camera_3d: CameraManager = $Camera3D
 @onready var book: BookManager = $Book
 
+@onready var wall_sprite: Sprite3D = $DemonWallSprite
+@onready var table_sprite: Sprite3D = $DemonTableSprite
+
+@export var test_demon: DemonInfo
+var curr_demon_info : DemonInfo
 
 func _ready() -> void:
 	gui.menu_button.pressed.connect(camera_3d.update_camera_mode.bind(ID.CameraMode.SHELF))
@@ -15,3 +19,12 @@ func _ready() -> void:
 	gui.menu_button.pressed.connect(book.close)
 	gui.table_button.pressed.connect(book.close)
 	
+	curr_demon_info = test_demon
+	setup_demon()
+
+func setup_demon() -> void:
+	wall_sprite.texture = curr_demon_info.shadow_texture
+	table_sprite.texture = curr_demon_info.shadow_texture
+	
+	#var tween = get_tree().create_tween()
+	#tween 
---
+++ code/demon_info.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name DemonInfo
+
+@export var name: String
+@export var type1: ID.DemonType = ID.DemonType.NONE
+@export var type2: ID.DemonType = ID.DemonType.NONE
+@export var shadow_texture: CompressedTexture2D
